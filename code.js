308808696e2c1c82fda2c8e3c9f38acc04c015f6
Nat Monique <96167252+ZeroMugen@users.noreply.github.com>
2023-03-28 08:11:04 +0800

    Add files via upload

---
+++ code.js
@@ -0,0 +1,17 @@
+function clk(val){
+
+    document.getElementById("screen").value=document.getElementById("screen").value+val;
+
+}
+
+function clr(){
+    document.getElementById("screen").value = ""
+}
+
+function eql(){
+
+    var text=document.getElementById("screen").value;
+    var result=eval(text);
+    document.getElementById("screen").value= result
+}
+
